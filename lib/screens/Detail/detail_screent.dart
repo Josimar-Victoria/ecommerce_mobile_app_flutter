53966959311e5ef1af8039fbd483113ba5060612
Josimar-Victoria <josimarvictoria968@gmail.com>
2024-09-27 23:09:37 -0500

    first commit

---
+++ lib/screens/Detail/detail_screent.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailScreent extends StatelessWidget {
+  const DetailScreent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
